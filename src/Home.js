d9e9b2571b7dc724b282df80dbd7915adbe217d9
thiagohernandez <thiago.hernandez@gmail.com>
2021-10-06 15:04:39 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import Card from './Components/Card/Card';
 
 const Home = ({data}) => {
+  const [filter, setFilter] = React.useState('');
+  function handleFilter(target) {
+    console.log(target);
+  }
+
     console.log(data);
   return (
       <div className="container animRight">
@@ -11,6 +16,11 @@ const Home = ({data}) => {
             <h1>All Docs</h1>
           </div>
           <div className="pageActions">
+            <select className="formControl" value={filter} id="type" onChange={handleFilter(event.target.value)}>
+                  <option value="Simple">Simple</option>
+                  <option value="Custom">Custom</option>
+                  <option value="Advanced">Advanced</option>
+              </select>
             <Link to='/create' className="btn btn-accent">Add new</Link>
           </div>
         </div>
